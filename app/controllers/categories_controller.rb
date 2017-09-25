a000e57552a0ad376a734b18eaf82381506db134
RicoDe Santis <ricodesantis@gmail.com>
2017-09-25 16:39:39 -0400

    build parts of nav and added category route

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,17 @@
+class CategoryController < ApplicationController
+  def Appetizers
+    @appetizers = Recipe.select {|obj| obj.category == "appetizer"}
+  end
+
+  def salads
+    @salads = Recipe.select {|obj| obj.category == "salad"}
+  end
+
+  def main_courses
+    @main_courses = Recipe.select {|obj| obj.category == "course"}
+  end
+
+  def desserts
+    @desserts = Recipe.select {|obj| obj.category == "dessert"}
+  end
+end
