7fd2f6116d8a1eda934c37cbcd1e14b853376780
Simon Brady <simonbrady@mac.com>
2017-09-25 17:09:12 -0400

    Build sessions controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Recipe.delete_all
+Review.delete_all
+
+def user_params
+  {
+    username:Faker::Seinfeld.character,
+    email:Faker::Internet.email,
+    passowrd:Faker::Internet.password
+  }
+end
+
+def categories
+  ["appetizer", "main_course", "dessert", "salad"].sample
+end
+
+def ingredients
+  Faker::Food.ingredient
+end
+
+def recipe_params
+  {
+    recipe_name:Faker::Food.dish,
+    category: categories.sample,
+    ingredients: Faker::Food.ingredient
+
+  }
