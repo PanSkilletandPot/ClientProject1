7fa17bdb21a73e4c62bfab121cf21d5b173cf65e
Simon Brady <simonbrady@mac.com>
2017-09-25 16:08:53 -0400

    Fixed the error in the migration file

--- db/migrate/20170925161535_create_recipes.rb
+++ db/migrate/20170925161535_create_recipes.rb
@@ -1,5 +1,5 @@
 class CreateRecipes < ActiveRecord::Migration[5.1]
-  def changeg
+  def change
     create_table :recipes do |t|
       t.string :recipe_name, null: false
       t.string :category, null: false
