1bead0ac3c0bee1ed975e13bc2341314089e58c8
Simon Brady <simonbrady@mac.com>
2017-09-25 14:46:00 -0400

    DB creation and migration

---
+++ db/migrate/20170925161535_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :recipes do |t|
+      t.string :recipe_name, null: false
+      t.string :category, null: false
+      t.string :ingredients, null: false
+      t.string :prep_time, null: false
+      t.string :instructions, null: false
+      t.references :user, foreign_key: true, index: true
+
+      t.timestamps
+    end
+  end
+end
