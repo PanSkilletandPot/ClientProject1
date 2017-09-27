b3e79fab655c09f16e6c6d9dd5d65d2ee4a70902
Simon Brady <simonbrady@mac.com>
2017-09-27 08:00:18 -0400

    Removed seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,3 @@
 User.delete_all
 Recipe.delete_all
 Review.delete_all
-
-def user_params
-  {
-    username:Faker::Seinfeld.character,
-    email:Faker::Internet.email,
-    passowrd:Faker::Internet.password
-  }
-end
-
-def categories
-  ["appetizer", "main_course", "dessert", "salad"].sample
-end
-
-def ingredients
-  Faker::Food.ingredient
-end
-
-def recipe_params
-  {
-    recipe_name:Faker::Food.dish,
-    category: categories.sample,
-    ingredients: Faker::Food.ingredient
-
-  }
